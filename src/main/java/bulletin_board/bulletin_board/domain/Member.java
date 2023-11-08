0b43b44a1192f8744d73be2ea2a8ab5082d5bf98
dexter.won <khw73850@gmail.com>
2023-11-08 13:51:40 +0900

    Initial commit

---
+++ src/main/java/bulletin_board/bulletin_board/domain/Member.java
@@ -0,0 +1,7 @@
+package bulletin_board.bulletin_board.domain;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Member {
+}
---
+++ src/test/java/bulletin_board/bulletin_board/BulletinBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package bulletin_board.bulletin_board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BulletinBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
